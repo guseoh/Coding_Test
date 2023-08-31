186c924da08788d7378cf1b0c7c92f5628527aed
guseoh <guseoh@naver.com>
2023-08-31 23:37:00 +0900

    2023-08-31

---
+++ Baekjoon/단계별로 풀어보기/1차원 배열/10871.py
@@ -0,0 +1,10 @@
+a, b = map(int, input().split())
+aa = list(map(int, input().split()))
+
+# for i in range(a):
+#     if(aa[i] > 5):
+#         aa.remove(aa[i])
+
+aa = str(aa)
+print(''.join(aa),sep='')
+
