949ca33ad91429b04f7d33f5acf983a316eaf3d5
guseoh <guseoh@naver.com>
2023-09-02 21:42:37 +0900

    2023-09-02

--- Baekjoon/단계별로 풀어보기/1차원 배열/10871.py
+++ Baekjoon/단계별로 풀어보기/1차원 배열/10871.py
@@ -1,10 +1,9 @@
 a, b = map(int, input().split())
 aa = list(map(int, input().split()))
 
-# for i in range(a):
-#     if(aa[i] > 5):
-#         aa.remove(aa[i])
+for i in range(a):
+    if(aa[i] < b):
+        print(aa[i], end=' ')
 
-aa = str(aa)
-print(''.join(aa),sep='')
 
+# IndexError: list index out of range: 리스트를 순회할 때 존재하지 않는 항목에 접근하려고 한다.
