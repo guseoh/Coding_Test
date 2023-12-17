0e791c0da8bbe9eba85ab5eba61766afb3786d0e
오지훈 <guseoh@naver.com>
2023-12-17 17:57:59 +0900

    2023-12-17

--- BarkingDog/기초코드 작성요령1/2480.py
+++ BarkingDog/기초코드 작성요령1/2480.py
@@ -1,4 +1,13 @@
-a,b,c = map(int, input().split())
+a = list(map(int, input().split()))
 
-if a == b == c:
-    print(10000 + a * 1000)
+if a[0] == a[1] == a[2]:
+    print(10000 + a[0] * 1000)
+elif a[0] == a[1]:
+    print(1000 + a[1] * 100)
+elif a[1] == a[2]:
+        print(1000 + a[1] * 100)
+elif a[0] == a[2]:
+        print(1000 + a[0] * 100)
+
+else:
+    print(max(a) * 100)
