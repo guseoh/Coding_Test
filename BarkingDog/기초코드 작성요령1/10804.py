98dff942b27ab28d8793abdd4b65cb92ec5c16c0
guseoh <guseoh@naver.com>
2023-12-18 13:35:59 +0900

    2023-12-18

--- BarkingDog/기초코드 작성요령1/10804.py
+++ BarkingDog/기초코드 작성요령1/10804.py
@@ -1,7 +1,14 @@
 # 카드 역배치
-x = [x for x in range(1,21)]
+x = [x for x in range(1,21)] # x = [*range(1,21)]
 
 for _ in range(10):
     a,b = map(int, input().split())
-    x[:a] + x[a:b+1:-1] + x[b:]
-    print(x)
+    x[a-1:b] =  x[a-1:b:][::-1]
+    
+print(*x)
+
+# n = [int(i) for i in range(1,21)]
+# for _ in range(10):
+#     a,b = map(int, input().split())
+#     n = n[:a-1] + n[a-1:b:][::-1] + n[b:]
+# print(" ".join(map(str, n)))
