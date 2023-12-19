299c70d02f8a20ed1f8941503d188465ec0078ef
오지훈 <guseoh@naver.com>
2023-12-19 21:11:09 +0900

    다시하기

--- BarkingDog/배열/2577.py
+++ BarkingDog/배열/2577.py
@@ -1,15 +1,25 @@
 # 숫자의 개수
 
-s = [0] * 10
+# s = [0] * 10
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# x = a * b * c
+
+# for i in str(x):
+#     s[int(i)] += 1
+
+# for x in s:
+#     print(x)
 
 a = int(input())
 b = int(input())
 c = int(input())
 
-x = a * b * c
+s = list(str(a*b*c))
+print(s)
 
-for i in str(x):
-    s[int(i)] += 1
-
-for x in s:
-    print(x)
+# for i in range(10):
+#     print(s.count(str(i)))
