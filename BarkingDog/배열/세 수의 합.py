0b39bf2e707f1de5446fe5676db769228d2a3047
오지훈 <guseoh@naver.com>
2023-12-27 19:22:36 +0900

    2023-12-27

--- BarkingDog/배열/세 수의 합.py
+++ BarkingDog/배열/세 수의 합.py
@@ -19,3 +19,32 @@ def threeSum(self, nums: list[int]) -> list[list[int]]:
             if nums[i] + nums[j] + nums[k] == 0:
                 result.append([nums[i], nums[j], nums[k]])
     return result
+
+# 풀이 2: 투 포인터
+def threeSum(self, nums: list[int]) -> list[list[int]]:
+    result = []
+    nums.sort()
+    
+    for i in range(len(nums) - 2):
+        # 중복된 값 건너뛰기
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        
+        # 간격을 좁혀가며 합 sum 계산
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            sum = nums[i] + nums[left] + nums[right]
+            if sum < 0:
+                left += 1
+            elif sum > 0:
+                left -= 1
+            else:
+                # sum = 0인 경우이므로 정답 및 스킵 처리
+                result.append([nums[i], nums[left], nums[right]])
+                
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
