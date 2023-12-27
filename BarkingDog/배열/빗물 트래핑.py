c6e7dbe2950116e215158deff967d1e6c9e606b1
오지훈 <guseoh@naver.com>
2023-12-27 18:53:04 +0900

    2023-12-27

--- BarkingDog/배열/빗물 트래핑.py
+++ BarkingDog/배열/빗물 트래핑.py
@@ -1,8 +1,9 @@
 # 높이를 입력받아 비 온 후 얼마나 많은 물이 쌓일 수 있는지 계산하라
 
 # 풀이 1: 투 포인터
-# 최대 높이의 막대까지 각각 좌우 기둥 최대 높이 left_max, right_max가 현재 높이와의
-# 차이만큼 물 높이 volume을 더해 나간다.
+# 가장 높이가 높은 막대를 보면, 빗물을 구한는데 있어, 막대의 높낮음은 무관하다.
+# => 그저 왼쪽과 오른쪽을 가르는 장벽과 같은 역할
+ 
 
 def trap(Self, height: list[int]) -> int:
     if not height:
@@ -23,3 +24,35 @@ def trap(Self, height: list[int]) -> int:
             volume += right_max - height[right]
             right -= 1
         return volume
+
+# 풀이 2: 스택
+# 스택에 높이를 쌓으면서, 현재 높이가 이전 높이보다 높을 때, 즉 변곡점을 기준으로 격차만큼
+# 물 높이 volume을 채운다.
+
+from typing import List
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        stack = []
+        volume = 0
+
+        for i in range(len(height)):
+            # 변곡점을 만나는 경우
+            while stack and height[i] > height[stack[-1]]:
+                # 스택에서 빼냄
+                top = stack.pop()
+
+                if not len(stack):
+                    break
+
+                # 이전과의 차이만큼 물 높이 처리
+                distance = i - stack[-1] - 1
+                waters = min(height[i], height[stack[-1]]) - height[top]
+
+                volume += distance * waters
+
+            stack.append(i)
+
+        return volume
+s = Solution()
+print(s.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
